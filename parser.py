b24ddfe527c179ad577261729a6c067f0189777b
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-11-11 21:36:37 +0000

    get mass

--- parser.py
+++ parser.py
@@ -1,25 +1,30 @@
 import xml.etree.ElementTree as ET
 
+# parse ork file
 tree = ET.parse("APEX.ork")
 root = tree.getroot()
 
+# split into physical attributes and simulation results
 rocket = root[0]
 sims = root[1]
 
-# for child in rocket:
-#     print(child.tag)
 
+# get motor
 for element in rocket.iter('motorconfiguration'):
     motor = element[0].text
 
-length = 0
+# sum mass components
+mass = 0
+
+for element in rocket.iter('overridemass'):
+    mass = mass + float(element.text)
+
+for element in rocket.iter('mass'):
+    mass = mass + float(element.text)
 
 
 
-
-print(motor)
-
-
+# extract simulation data
 apogee = sims[0].find('flightdata').attrib['maxaltitude']
 maxvel = apogee = sims[0].find('flightdata').attrib['maxvelocity']
 maxacc = sims[0].find('flightdata').attrib['maxacceleration']
