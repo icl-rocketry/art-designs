7e7c12136c33637fa839a3aa5e2afc79c0f21460
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-11-12 17:08:36 +0000

    more shell enable

--- parser.py
+++ parser.py
@@ -112,8 +112,8 @@ def create_commit(design = "APEX"):
     info = f"README updated after commit {hash} by {author}"
     print(info)
 
-    subprocess.run(f"git commit -am \"{info}\"")
-    subprocess.run("git push origin HEAD")
+    subprocess.run(f"git commit -am \"{info}\"", shell=True)
+    subprocess.run("git push origin HEAD", shell=True)
     
 create_commit("APEX")
 create_commit("ASCENSION")
